a6ecb64ee58cc8bfdb7c7ee23779b9f1fe77c8d1
Tatyana Komarova <komarovats93@gmail.com>
2018-05-28 15:47:55 +0300

    Секция с зелёными категориями

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
     
-    
+    $('.green_cat').mouseover(function() {
+        if (!$(this).hasClass('active')) {
+            $('.green_cat.active').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+    $('.green_cat').mouseleave(function() {
+        if ($(this).index != 1) {
+            $(this).removeClass('active');
+            $('.green_cat').eq(1).addClass('active');
+        }
+    });
+
 });
