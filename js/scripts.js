eed6f33ab99d39b935a6e3e9c649596207d17139
unknown <keitmark@mail.ru>
2018-06-02 15:50:29 +0300

    save

--- js/scripts.js
+++ js/scripts.js
@@ -39,7 +39,26 @@ $(document).ready(function () {
     });
     
     
+    //инпут файл
     
+    var inputs = document.querySelectorAll('.inputfile');
+    Array.prototype.forEach.call(inputs, function (input) {
+        var label = input.nextElementSibling
+            , labelVal = label.innerHTML;
+        input.addEventListener('change', function (e) {
+            var fileName = '';
+            if (this.files && this.files.length > 1) fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+            else fileName = e.target.value.split('\\').pop();
+            if (fileName) {
+                label.querySelector('span').innerHTML = fileName;
+                $(".buttons-wrap-chat label").addClass("was-add");
+            }
+            else {
+                label.innerHTML = labelVal;
+                $(".buttons-wrap-chat label").removeClass("was-add");
+            }
+        });
+    });
     
     
     
